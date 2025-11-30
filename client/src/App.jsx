55f15178b86fdd3b3683e9f99443d8b5b42728ab
Sivasindhuja <sindhutsundupalli@gmail.com>
2025-11-30 12:45:42 +0530

    map function to dynamically render the notes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Note from "./components/Note";
+import notes from "./Notes.js";
+
+function renderNote(note){
+    return (
+      <Note noteId={note.notesid} title={note.noteTitle} content={note.noteContent}/>
+    )
+}
 function App(){
   return (
     <div>
       <Header/>
-      <Note/>
+      {notes.map(renderNote)}
       <Footer/>
     </div>
   )
---
+++ client/src/Notes.js
@@ -0,0 +1,21 @@
+const notes=[
+    {
+        notesid:"1",
+        noteTitle:"title1",
+        noteContent:"content1",
+
+    },
+    {
+        notesid:"2",
+        noteTitle:"title2",
+        noteContent:"content2",
+
+    },
+    {
+        notesid:"3",
+        noteTitle:"title3",
+        noteContent:"content3",
+
+    }
+];
+export default notes;
