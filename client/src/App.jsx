1142debddbf68fa17f581b3d26a787cabdcd5534
Sivasindhuja <sindhutsundupalli@gmail.com>
2025-12-29 07:56:36 +0530

    changed frontend to call the backend hosted in the AWS ec2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     // Define the function INSIDE the effect
     const fetchNotes = async () => {
         try {
-            const res = await axios.get(`http://localhost:5000/api/notes?userId=${userId}`);
+            const res = await axios.get(`http://13.48.177.41:5000/api/notes?userId=${userId}`);
             setNotes(res.data);
         } catch (err) {
             console.error("Fetch failed:", err);
@@ -25,7 +25,7 @@ function App() {
 }, []); // Empty dependency array ensures it runs once on mount
     // CREATE
     async function addNote(newNote) {
-        const res = await axios.post("http://localhost:5000/api/notes", {
+        const res = await axios.post("http://13.48.177.41:5000/api/notes", {
             ...newNote,
             userId: userId
         });
@@ -34,7 +34,7 @@ function App() {
 
     // DELETE
     async function deleteNote(id) {
-        await axios.delete(`http://localhost:5000/api/notes/${id}`);
+        await axios.delete(`http://13.48.177.41:5000/api/notes/${id}`);
         setNotes(prevNotes => prevNotes.filter(n => n.id !== id));
     }
 
